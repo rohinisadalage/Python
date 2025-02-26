0abfbaf9426a5deee5cd89c3ef8c79ba6a97db1d
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2025-02-26 19:42:04 +0530

    Add files via upload

---
+++ pos neg zero.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the number: "));
+if a>0:
+    print("The number is Positive.");
+elif a<0:
+    print("The number is Negative.");
+else:
+    print("The number is Zero.");
+print("Thank You...");
