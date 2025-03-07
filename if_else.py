fbb1d596026235f4fa8dc4906b67ce83e726abd5
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2025-03-07 19:09:52 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the 1st Number:"));
+b=int(input("Enter the 2nd Number:"));
+print(a);
+if a>b:
+    print("The max number is:",a);
+else:
+     print("The max number is:",b);
+print("Thank You...");
