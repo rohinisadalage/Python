d4f96240ec769a52e7780c24669acc20d69d5d00
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2025-03-10 20:17:07 +0530

    Add files via upload

---
+++ even odd.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the any No. those you check the number is even or odd: "));
+print(a);
+if a%2==0:
+   print("The number is even number.");
+else:
+   print("The number is odd number.");
+print("Thank You...");
